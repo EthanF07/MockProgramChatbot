746b7ed0e2939bde767ac9e8fbbcd5d4629689cd
EthanF07 <ethanfang50@gmail.com>
2023-12-01 13:11:58 -1000

    Made main page and linking

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,6 +19,9 @@ export default function Home() {
             Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
           </h1>
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
+            <h1 className = "text-white">
+            Read <Link href="/posts/main-page">this page!</Link>
+            </h1>
             <Link
               className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
               href="https://create.t3.gg/en/usage/first-steps"
