ae0165fe61b6206524a4515646bca79f2bd6484b
EthanF07 <ethanfang50@gmail.com>
2023-12-04 14:27:27 -1000

    Changed Header text for chatbot page

--- src/pages/posts/chatbot-page.js
+++ src/pages/posts/chatbot-page.js
@@ -1,9 +1,15 @@
 import Link from 'next/link';
+import Head from 'next/head'
 
 export default function ChatbotPage() {
     return (
     <>
-        <h1 className = "badge">ChatbotPage</h1>
+        <Head>
+        <title>ChatbotPage</title>
+        </Head>
+        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+            <span className="text-[hsl(280,100%,70%)]">C.O.L.B. Chatbot</span>
+        </h1>
         <h2>
             <Link href="main-page">To Home</Link>
         </h2>
